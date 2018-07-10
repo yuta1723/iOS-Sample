e9068f0f243a65ad055f6258c192a4b559a76434
yuta naito <y.naito@suvt.co.jp>
2018-07-11 06:54:14 +0900

    ボタンを追加

--- iOS-samples/ViewController.m
+++ iOS-samples/ViewController.m
@@ -18,6 +18,15 @@
     NSLog(@"viewDidLoad");
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    // ボタンを設定
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    button.frame = CGRectMake(0,60,100,30);
+    [button setTitle:@"ボタン" forState:UIControlStateNormal];
+    [button setTitle:@"ハイライト" forState:UIControlStateHighlighted];
+    [button setTitle:@"無効" forState:UIControlStateDisabled];
+    
+    [self.view addSubview:button];
 }
 
 
