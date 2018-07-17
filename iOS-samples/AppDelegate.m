80f7a5845a1124d47ac57f55fe4adcb63f4cb464
yuta naito <y.naito@suvt.co.jp>
2018-07-18 03:30:10 +0900

    NavigationControllerをルートのViewに設定

--- iOS-samples/AppDelegate.m
+++ iOS-samples/AppDelegate.m
@@ -21,9 +21,12 @@
     
     // Override point for customization after application launch.
     
+    UIViewController *viewController = [[ViewController alloc] init];
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:viewController];
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.window.rootViewController = [[ViewController alloc] init];
+    self.window.rootViewController = navigationController;
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     
